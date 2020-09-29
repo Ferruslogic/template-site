fa382ea26bd7b0da8c1238a939d8fec1f665dabb
andresdt <torocruzand@gmail.com>
2020-09-28 22:55:24 -0400

    [ADD] Carpetas para el idioma, data y servicio del api

---
+++ assets/src/api/api.services.js
@@ -0,0 +1,87 @@
+function API_getRequest(id) {
+    axios.get('http://localhost:8080/item/' + id)
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {});
+}
+
+function API_getFilteredRequest(filter) {
+    axios.get('http://localhost:8080/item', {
+            params: {
+                filter: filter
+            }
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {});
+}
+
+function API_getAllRequest() {
+    axios.get('http://localhost:8080/item')
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {});
+}
+
+function API_postRequest(data) {
+    axios.post('http://localhost:8080/item', {
+            data: data
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {});
+}
+
+function API_putRequest(id, data) {
+    axios.put('http://localhost:8080/item/ ' + id, {
+            data: data
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {});
+}
+
+function API_patchRequest(id, data) {
+    axios.patch('http://localhost:8080/item/ ' + id, {
+            data: data
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {});
+}
+
+
+function API_deleteRequest(id) {
+    axios.delete('http://localhost:8080/item/' + id)
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {});
+}
