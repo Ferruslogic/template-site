78a0b231b53ca14f5b007cbedbe5c7e6a6fcabf0
andresdt <torocruzand@gmail.com>
2020-10-03 16:53:09 -0400

    libreria md

--- assets/src/pages/about.vue.js
+++ assets/src/pages/about.vue.js
@@ -2,12 +2,11 @@ var pageAbout = {
     template: `
     <div>
     <h1>About</h1> <br><br>
-    <h1>tegnologis</h1> <br><br>
-    <h1> Proyectos realizados ###</h1>  <br><br>
-    <h1> Proyectos en curso ###</h1> <br>
-    <h1> Proyectos en cerrados ###</h1> <br>
-    <h1> Proyectos con soporte ###</h1> <br>
-                    <testcom></testcom>
+   <p>
+   Durante aproximadamente 12 años hemos estado desarrollando software, plugin, librerías y más recientemente widgets. En el 2020 decidimos que ya era hora de compartir con la comunidad un poco de lo que hemos creado. Así nace la idea de este Blog.
+Aquí podrás encontrar algunos de nuestros proyectos, consejos trucos y un poco de nosotros. 
+Advertencia: El contenido que se publica en este foro se hace desde un punto de vista muy personal. Sin intenciones de ofender a nadie.
+   </p>
 
     </div>
 `
