8c5334aa27c122e4d695399f3a084a2591efc44f
andresdt <torocruzand@gmail.com>
2020-10-04 00:49:26 -0400

    listado de post

--- assets/src/components/base-component.vue.js
+++ assets/src/components/base-component.vue.js
@@ -203,3 +203,56 @@ Vue.component('base-footer', {
         ],
     }),
 });
+
+
+Vue.component('base-grid-post', {
+    template: `
+  <div>
+    <v-card class="mx-auto" max-width="344" outline>
+        <v-img :src="thumbnail" height="200px"></v-img>
+
+        <v-card-title>
+            {{title}}
+        </v-card-title>
+
+        <v-card-subtitle>
+             {{subtitle}}
+        </v-card-subtitle>
+
+        <v-card-actions>
+            <a :href="postLink">
+            <v-btn color="orange lighten-2" text>
+                
+                Explore
+            </v-btn>
+            </a>
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+            <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                  {{ content }}
+                </v-card-text>
+            </div>
+        </v-expand-transition>
+    </v-card>
+</div>
+`,
+    data: () => ({
+        show: false
+    }),
+    props: {
+        title: String,
+        content: String,
+        thumbnail: String,
+        postLink: String,
+        subtitle: String
+    }
+})
--- assets/src/pages/blog.vue.js
+++ assets/src/pages/blog.vue.js
@@ -3,67 +3,36 @@ var pagBlog = {
     <div>
     <div class="title">Blog</div>
 
-<li v-for="post in posts">
-    <h2><router-link v-bind:to="'/blog/'+post.id">{{post.title}}</router-link></h2>
-    <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
-
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn
-        color="orange lighten-2"
-        text
-      >
-        Explore
-      </v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
-
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
-</template>
-</li>
+<template>
+  <v-item-group>
+    <v-container>
+      <v-row>
+        <v-col v-for="(post, index) in posts"
+        :key ="index"
+         cols="12"md="4" sm="6" >         
+          <v-item>
+           <base-grid-post  
+           :title="post.title"
+           :thumbnail="post.thumbnail"
+           :postLink="post.postLink"
+           :content="post.content"
+         />
+            </v-item>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-item-group>
+</template> 
     </div>
     `,
     data: () => ({
         /* ToDo: La lista de post se las pide a el api, esto es para probar */
         posts: [
-            { id: 'noticia-1', title: 'Noticia 1', content: 'Lorem ipsum dolor sit amet 1' },
-            { id: 'noticia-2', title: 'Noticia 2', content: 'Lorem ipsum dolor sit amet 2' },
-            { id: 'noticia-3', title: 'Noticia 3', content: 'Lorem ipsum dolor sit amet 3' },
-            { id: 'noticia-4', title: 'Noticia 4', content: 'Lorem ipsum dolor sit amet 4' }
-        ],
-        show: false
+            { id: 'noticia-1', title: 'Noticia 1', content: 'Lorem ipsum dolor sit amet 1', postLink: '#', thumbnail: './assets/public/images/posts/00/_thumbnail.jpg' },
+            { id: 'noticia-2', title: 'Noticia 2', content: 'Lorem ipsum dolor sit amet 2', postLink: '#', thumbnail: './assets/public/images/posts/00/_thumbnail.jpg' },
+            { id: 'noticia-3', title: 'Noticia 3', content: 'Lorem ipsum dolor sit amet 3', postLink: '#', thumbnail: './assets/public/images/posts/00/_thumbnail.jpg' },
+            { id: 'noticia-4', title: 'Noticia 4', content: 'Lorem ipsum dolor sit amet 4', postLink: '#', thumbnail: './assets/public/images/posts/00/_thumbnail.jpg' }
+        ]
     })
 
 };
