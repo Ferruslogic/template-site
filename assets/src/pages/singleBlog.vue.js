105cfb5ae9557d5334384bf30851fe26776abc2f
andresdt <torocruzand@gmail.com>
2020-10-04 01:11:43 -0400

    titulo del post

--- assets/src/pages/singleBlog.vue.js
+++ assets/src/pages/singleBlog.vue.js
@@ -1,8 +1,8 @@
 var singleBlog = {
     template: `
     <div> 
-        <div class="title">Post </div>
-    {{ $.router.params.id }}
+        <div class="title"> {{ this.$route.params.id }} </div>
+    
 <p> AQUI ES DONDE SE CARGA EL HTML O EL MD! </p>
     v-html="html" <-- Toy probando con eso que no funciona.
   </div>
