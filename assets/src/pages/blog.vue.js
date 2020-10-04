f676b236afbfa673970667df2d9115ff801dca1d
andresdt <torocruzand@gmail.com>
2020-10-03 23:22:15 -0400

    comanezando a listar los post con un arreglo quemado.

--- assets/src/pages/blog.vue.js
+++ assets/src/pages/blog.vue.js
@@ -2,31 +2,68 @@ var pagBlog = {
     template: `
     <div>
     <div class="title">Blog</div>
-    <router-link to="/about">About us</router-link>
-        <v-card class="elevation-2">
-    <v-card-title class="headline"> Etre heureux</v-card-title>
-    <v-card-text>
-      Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
-    </v-card-text>
-  </v-card>
-</v-timeline-item>
-</v-timeline>
 
 <li v-for="post in posts">
     <h2><router-link v-bind:to="'/blog/'+post.id">{{post.title}}</router-link></h2>
+    <template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      Top western road trips
+    </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
 </li>
     </div>
     `,
-    data() {
-        return {
-            posts: [
+    data: () => ({
+        /* ToDo: La lista de post se las pide a el api, esto es para probar */
+        posts: [
+            { id: 'noticia-1', title: 'Noticia 1', content: 'Lorem ipsum dolor sit amet 1' },
+            { id: 'noticia-2', title: 'Noticia 2', content: 'Lorem ipsum dolor sit amet 2' },
+            { id: 'noticia-3', title: 'Noticia 3', content: 'Lorem ipsum dolor sit amet 3' },
+            { id: 'noticia-4', title: 'Noticia 4', content: 'Lorem ipsum dolor sit amet 4' }
+        ],
+        show: false
+    })
 
-                { id: 'noticia-1', title: 'Noticia 1', content: 'Lorem ipsum dolor sit amet 1' },
-                { id: 'noticia-2', title: 'Noticia 2', content: 'Lorem ipsum dolor sit amet 2' },
-                { id: 'noticia-3', title: 'Noticia 3', content: 'Lorem ipsum dolor sit amet 3' },
-                { id: 'noticia-4', title: 'Noticia 4', content: 'Lorem ipsum dolor sit amet 4' }
-
-            ]
-        }
-    }
 };
--- assets/src/pages/singleBlog.vue.js
+++ assets/src/pages/singleBlog.vue.js
@@ -2,6 +2,13 @@ var singleBlog = {
     template: `
     <div> <div class="title">Post</div>
     
+<p> AQUI ES DONDE SE CARGA EL HTML O EL MD! </p>
+    v-html="html" <-- Toy probando con eso que no funciona.
   </div>
-    `
+    `,
+    data() {
+        return {
+            html: '<p> HTML LINCK TEST</p>'
+        }
+    },
 };
